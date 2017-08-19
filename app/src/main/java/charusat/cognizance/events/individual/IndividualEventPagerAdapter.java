4fe98c7cb56dcf3b725355e267d209101d523b21
AGENT-007\Mit Patel <mitpatel23897@gmail.com>
2017-08-19 09:23:29 +0530

    Bug Fixes and Improvements.

--- app/src/main/java/charusat/cognizance/events/individual/IndividualEventPagerAdapter.java
+++ app/src/main/java/charusat/cognizance/events/individual/IndividualEventPagerAdapter.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.CardView;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bluejamesbond.text.DocumentView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -50,7 +53,7 @@ class IndividualEventPagerAdapter extends PagerAdapter
     private TextView title; //name
     private TextView one_line_description;
     private TextView date_time;
-    private TextView detail_description;
+    private DocumentView detail_description;
     private TextView venue;
     private TextView rules;
     private TextView number_of_participants;
@@ -65,7 +68,7 @@ class IndividualEventPagerAdapter extends PagerAdapter
         title = (TextView) itemView.findViewById(R.id.title);
         one_line_description = (TextView) itemView.findViewById(R.id.description_one_line);
         date_time = (TextView) itemView.findViewById(R.id.date_and_time);
-        detail_description = (TextView) itemView.findViewById(R.id.detail_description);
+        detail_description = (DocumentView) itemView.findViewById(R.id.detail_description);
         venue = (TextView) itemView.findViewById(R.id.venue_of_event);
         rules = (TextView) itemView.findViewById(R.id.rules);
         number_of_participants = (TextView) itemView.findViewById(R.id.number_of_participants);
@@ -74,13 +77,13 @@ class IndividualEventPagerAdapter extends PagerAdapter
         EventHolder event = events.get(position);
 
         title.setText(event.name);
-        one_line_description.setText(event.one_line_description);
+        one_line_description.setText(Html.fromHtml(event.one_line_description));
         date_time.setText(event.date_time);
-        detail_description.setText(event.detail_description);
+        detail_description.setText(Html.fromHtml(event.detail_description));
         venue.setText(event.venue);
-        rules.setText(event.rules);
+        rules.setText(Html.fromHtml(event.rules));
         number_of_participants.setText(event.number_of_participants);
-        fees.setText(event.fees);
+        fees.setText(Html.fromHtml(event.fees));
 
 
 
