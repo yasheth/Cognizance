6b6d0909e159c243c6e8285a0b3db35fe4ee81a7
Yash Sodha <yashsodha@gmail.com>
2017-08-26 23:00:40 +0530

    Firebase Setup

--- app/src/main/java/charusat/cognizance/UserFragment.java
+++ app/src/main/java/charusat/cognizance/UserFragment.java
@@ -2,10 +2,21 @@ package charusat.cognizance;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -13,6 +24,12 @@ import android.view.ViewGroup;
  */
 public class UserFragment extends Fragment {
 
+    final String TAG = "Firebase";
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference();
+    EditText czid;
+    EditText mobile;
+    Button status;
 
     public UserFragment() {
         // Required empty public constructor
@@ -26,4 +43,25 @@ public class UserFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        czid = (EditText) view.findViewById(R.id.et_czid);
+        mobile = (EditText) view.findViewById(R.id.et_cz_mobile);
+        status = (Button) view.findViewById(R.id.bt_status);
+
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cz_id;
+                String cz_mobile;
+                cz_id = czid.getText().toString();
+                cz_mobile = mobile.getText().toString();
+                final String participant = cz_id+"_"+cz_mobile;
+                Log.d(TAG, participant);
+            }
+        });
+
+    }
 }
