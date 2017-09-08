5abadbfaff9f81e6b71880d452346ebe7c1023cb
AGENT-007\Mit Patel <mitpatel23897@gmail.com>
2017-09-08 21:23:28 +0530

    Yash - Initial Maps Commit

--- app/src/main/java/charusat/cognizance/home/CustomPagerAdapter.java
+++ app/src/main/java/charusat/cognizance/home/CustomPagerAdapter.java
@@ -8,9 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import com.squareup.picasso.Picasso;
-
 import charusat.cognizance.R;
 
 /**
--- app/src/main/java/charusat/cognizance/home/HomeFragment.java
+++ app/src/main/java/charusat/cognizance/home/HomeFragment.java
@@ -20,7 +20,14 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 
@@ -33,7 +40,8 @@ import charusat.cognizance.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements
+        GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
 
     //RECYCLER VIEW FIELDS
     RecyclerView recyclerView,recyclerView2;
@@ -109,9 +117,61 @@ public class HomeFragment extends Fragment {
         }
         setUpPager(v);
 
+
+        SupportMapFragment mapFragment = (SupportMapFragment) view.findViewById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         socialButtons();
+
+    }
+    /** Called when the map is ready. */
+    @Override
+    public void onMapReady(GoogleMap map) {
+        /*mMap = map;
+
+        // Add some markers to the map, and add a data object to each marker.
+        mPerth = mMap.addMarker(new MarkerOptions()
+                .position(PERTH)
+                .title("Perth");
+        mPerth.setTag(0);
+
+        mSydney = mMap.addMarker(new MarkerOptions()
+                .position(SYDNEY)
+                .title("Sydney");
+        mSydney.setTag(0);
+
+        mBrisbane = mMap.addMarker(new MarkerOptions()
+                .position(BRISBANE)
+                .title("Brisbane");
+        mBrisbane.setTag(0);
+
+        // Set a listener for marker click.
+        mMap.setOnMarkerClickListener(this);*/
     }
 
+    /** Called when the user clicks a marker. */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Retrieve the data from the marker.
+        Integer clickCount = (Integer) marker.getTag();
+
+        // Check if a click count was set, then display the click count.
+        if (clickCount != null) {
+            clickCount = clickCount + 1;
+            marker.setTag(clickCount);
+            Toast.makeText(getContext(),
+                    marker.getTitle() +
+                            " has been clicked " + clickCount + " times.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
